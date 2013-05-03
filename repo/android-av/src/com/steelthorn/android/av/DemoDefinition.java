d24a4a09cab0467bf2e569b6322a181b27a380f8
zaventh <jeff.mixon@steelthorn.com>
2013-05-03 11:50:30 -0700

    apply gplv2 license

--- repo/android-av/src/com/steelthorn/android/av/DemoDefinition.java
+++ repo/android-av/src/com/steelthorn/android/av/DemoDefinition.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Jeff Mixon.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v2.0
+ * (or any later version, at your option)  which accompanies this distribution,
+ * and is available at http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * 
+ * Contributors:
+ *     Jeff - initial API and implementation
+ ******************************************************************************/
 package com.steelthorn.android.av;
 
 class DemoDefinition implements IScanDefinition
